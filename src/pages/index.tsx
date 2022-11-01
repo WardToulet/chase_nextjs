76d57eb637a14af10d301def547fb2f27ecea35a
Ward Toulet <ward@toulet.net>
2022-11-01 15:23:33 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      hello chasers
+    </>
+  );
+};
+
+export default Home;
+
