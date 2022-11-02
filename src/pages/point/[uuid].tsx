9384d0c00369ccc33af668c774c7a9f7c955021c
Ward Toulet <ward@toulet.net>
2022-11-02 12:20:23 +0100

    [fix] Moved logic into client side

--- src/pages/point/[uuid].tsx
+++ src/pages/point/[uuid].tsx
@@ -1,22 +1,19 @@
-import { prisma } from '../../server/db/client';
+import { useRouter } from 'next/router';
+import { trpc } from '../../utils/trpc';
 
 export async function getServerSideProps(context: { params: { uuid: string }}) {
-  const point = await prisma?.point.findFirst({ where: { uuid: context.params.uuid }});
-
-  // If we try to capture a non existing point we redirect to the register endpoint
-  if (!point) {
-    return {
-      redirect: {
-        destination: `/point/register/${context.params.uuid}`
-      },
-    }
-  }
-
   return {
     props: { uuid: context.params.uuid }
   }
 }
 
 export default function Point({ uuid }: { uuid: string }) {
-  return <>{uuid}</>
+  const router = useRouter();
+  const pointQuery = trpc.point.get.useQuery({ uuid });
+    
+  if (pointQuery.isSuccess && !pointQuery.data) {
+    router.replace(`/point/register/${uuid}`);
+  }
+
+  return <>{ uuid }</>
 }
